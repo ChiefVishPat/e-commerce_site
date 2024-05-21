6f73fa73c441f7d12f8a0b601ca8570c74af8784
Vishal Patel - personal <chiefvishpat@gmail.com>
2024-05-21 13:52:41 -0400

    fixed a small bug in ecom/urls.py regarding media. Added in some of the models required for the store

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -23,5 +23,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('store.urls')),
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #connecting the media stuff added in the settings
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #connecting the media stuff added in the settings
--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+#categories of products
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=11)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+    
+#all of our products
+class Product(models.Model):
+    prod_name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6) #ex of max digits: 9999.99
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+
+    def __str__(self) -> str:
+        return self.prod_name
+
+#customer orders
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True, null=True)
+    phone = models.CharField(max_length=11, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.product
