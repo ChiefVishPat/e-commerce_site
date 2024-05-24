954b790411702c071cba4947a2113d328f2c85d3
Vishal Patel - personal <chiefvishpat@gmail.com>
2024-05-24 17:10:44 -0400

    created a new app named cart. Added in the relevant urls and views to get to the cart page

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('store.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #connecting the media stuff added in the settings
