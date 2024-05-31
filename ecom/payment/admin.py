1cd4dd0914a50ad5a00ae6761debd08c991c4805
Vishal Patel - personal <chiefvishpat@gmail.com>
2024-05-31 10:26:47 -0400

    added in backend on admin page orders being marked with a date when marked as shipped

--- ecom/payment/admin.py
+++ ecom/payment/admin.py
@@ -16,6 +16,8 @@ class OrderItemInline(admin.StackedInline):
 #extend order model
 class OrderAdmin(admin.ModelAdmin):
     model = Order
+    readonly_fields = ["date_ordered"]
+    fields = ["user", "full_name", "email", "shipping_address", "amount_paid", "date_ordered", "shipped", "date_shipped"]
     inlines = [OrderItemInline]
 
 
