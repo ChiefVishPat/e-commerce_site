ea77745ef4975730c930a7db1fbcfaf90af0158f
Vishal Patel - personal <chiefvishpat@gmail.com>
2024-05-31 09:46:14 -0400

    added in on admin site the ability to view items in an order and itemized pricing

--- ecom/payment/admin.py
+++ ecom/payment/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 
 #register model on admin site
 admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+#create an OrderItem inline
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+
+#extend order model
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [OrderItemInline]
+
+
+admin.site.unregister(Order)
+admin.site.register(Order, OrderAdmin)
